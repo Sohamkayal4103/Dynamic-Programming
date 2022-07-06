a1a742a08328431129daec58d578ac968fe5e563
Soham <kayalsoham61@gmail.com>
2022-07-06 09:58:53 +0530

    Frog Jump Problem using Tabulation(Space Optimized)

--- frogJumpTabulation.cpp
+++ frogJumpTabulation.cpp
@@ -17,6 +17,26 @@ int frogJump(int n, vector<int> &heights)
     return dp[n-1];
 }
 
+//frogJump space optimized
+int frogJumpSpaceOptimized(int n, vector<int> &heights)
+{
+    
+    int prev,prev2,curr;
+    prev = 0;
+    prev2 = INT_MAX;
+    for(int i = 1;i < n;i++){
+        int ls = prev+abs(heights[i]-heights[i-1]);
+        int rs = INT_MAX;
+        if(i > 1){
+            rs = prev2+abs(heights[i]-heights[i-2]);
+        }
+        curr = min(ls,rs);
+        prev2 = prev;
+        prev = curr;
+    }
+    return curr;
+}
+
 int main() {
   ll t;
   cin >> t;

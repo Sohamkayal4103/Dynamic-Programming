e090c0963c593ca4608f2ee995eab1362ab08dbb
Soham <kayalsoham61@gmail.com>
2022-07-08 23:34:14 +0530

    Max Sum of non-adj elements(Using Tabulation and Space optimiztion)

--- MaxSumofNonAdjTabulation.cpp
+++ MaxSumofNonAdjTabulation.cpp
@@ -20,25 +20,25 @@ int maximumNonAdjacentSum(vector<int> &nums){
 
 //space -optimized solution
 
-// int maximumNonAdjacentSum(vector<int> &nums){
-//     int n = nums.size();
+int maximumNonAdjacentSum(vector<int> &nums){
+    int n = nums.size();
     
-//     int prev,prev2,curr;
+    int prev,prev2,curr;
     
-//     prev = nums[0];
-//     prev2 = 0;
-//     for(int i = 1;i < n;i++){
-//         int take = nums[i];
-//         if(i > 1){
-//             take += prev2;
-//         }
-//         int notTake = 0 + prev;
-//         curr = max(take,notTake);
-//         prev2 = prev;
-//         prev = curr;
-//     }
-//     return prev;
-// }
+    prev = nums[0];
+    prev2 = 0;
+    for(int i = 1;i < n;i++){
+        int take = nums[i];
+        if(i > 1){
+            take += prev2;
+        }
+        int notTake = 0 + prev;
+        curr = max(take,notTake);
+        prev2 = prev;
+        prev = curr;
+    }
+    return prev;
+}
 
 
 int main(){
